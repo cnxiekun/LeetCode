d6e09ad2c47a1b054b9dd20e484eb2f38871f285
cnxiekun <cnxiekun@gmail.com>
2020-03-06 12:02:24 +0800

    Update ==> 添加链表é¢相关目

---
+++ [面试题24]反转链表.py
@@ -0,0 +1,36 @@
+# 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。 
+# 
+#  
+# 
+#  示例: 
+# 
+#  输入: 1->2->3->4->5->NULL
+# 输出: 5->4->3->2->1->NULL 
+# 
+#  
+# 
+#  限制： 
+# 
+#  0 <= 节点个数 <= 5000 
+# 
+#  
+# 
+#  注意：本题与主站 206 题相同：https://leetcode-cn.com/problems/reverse-linked-list/ 
+#  Related Topics 链表
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        reversed_head = None
+        current = head
+        while current:
+            reversed_head, reversed_head.next, current = current, reversed_head, current.next
+        return reversed_head
+# leetcode submit region end(Prohibit modification and deletion)
