6138b3cd875f7125a1a2ec816fdc7ccd488e93ab
Kun XIE <kunixe@tencent.com>
2021-08-13 16:42:28 +0800

    Update ==> 更新82迭代解法

--- 82.删除排序链表中的重复元素-ii.py
+++ 82.删除排序链表中的重复元素-ii.py
@@ -14,8 +14,8 @@ class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
         # """递归"""
         # # 166/166 cases passed (52 ms)
-        # # Your runtime beats 26.85 % of python3 submissions
-        # # Your memory usage beats 56.18 % of python3 submissions (15 MB)
+        # # Your runtime beats 96.73 % of python3 submissions
+        # # Your memory usage beats 59.2 % of python3 submissions (15 MB)
         # if not head or not head.next:
         #     return head
         # if head.val != head.next.val:
@@ -28,17 +28,18 @@ class Solution:
         # return head
         
         """迭代"""
+        # 166/166 cases passed (36 ms)
+        # Your runtime beats 89.08 % of python3 submissions
+        # Your memory usage beats 9.18 % of python3 submissions (15.1 MB)
         if not head or not head.next:
             return head
         res = current = ListNode(0)
         res.next = head
         while head and head.next:
             if head.val == head.next.val:
-                move = head.next
-                while move and move.next:
-                    if move.val == move.next.val:
-                        move = move.next
-                head = move.next
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
                 current.next = head
             else:
                 current = current.next
