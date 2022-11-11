dd83f198bfefdbb07daf2c3b0e93db98fd22c02b
Kun XIE <kunixe@tencent.com>
2022-11-11 15:51:15 +0800

    Update ==> 26 快慢指针 删除有序列表中的重复项

--- 26.删除有序数组中的重复项.py
+++ 26.删除有序数组中的重复项.py
@@ -10,13 +10,22 @@ class Solution:
         # 362/362 cases passed (40 ms)
         # Your runtime beats 84.32 % of python3 submissions
         # Your memory usage beats 98.28 % of python3 submissions (15.4 MB)
-        res = 1
-        for i in range(len(nums) - 1):
-            if nums[i] != nums[i+1]:
-                nums[res] = nums[i+1]
-                res += 1
-        nums = nums[:res]
-        return res
+        # res = 1
+        # for i in range(len(nums) - 1):
+        #     if nums[i] != nums[i+1]:
+        #         nums[res] = nums[i+1]
+        #         res += 1
+        # nums = nums[:res]
+        # return res
+        if not nums:
+            return 0
         
+        slow, fast = 0, 0
+        while fast < len(nums):
+            if nums[slow] != nums[fast]:
+                slow += 1
+                nums[slow] = nums[fast]
+            fast += 1
+        return slow + 1 
 # @lc code=end
 
