56534c9d3c64914592c2ea722398878480c888ab
Kun XIE <kunixe@tencent.com>
2022-11-14 09:59:43 +0800

    Update ==> 83 新增快慢指针解法 删除排序链表中的重复元素

--- 83.删除排序链表中的重复元素.py
+++ 83.删除排序链表中的重复元素.py
@@ -16,14 +16,22 @@ class Solution:
         # 166/166 cases passed (44 ms)
         # Your runtime beats 65.47 % of python3 submissions
         # Your memory usage beats 34.51 % of python3 submissions (15 MB)
-        curr = head
-        while curr:
-            while curr.next and curr.val == curr.next.val:
-                curr.next = curr.next.next
-            curr = curr.next
-        return head
-
-
+        # curr = head
+        # while curr:
+        #     while curr.next and curr.val == curr.next.val:
+        #         curr.next = curr.next.next
+        #     curr = curr.next
+        # return head
+        if not head:
+            return head
         
+        slow, fast = head, head
+        while fast:
+            if fast.val != slow.val:
+                slow.next = fast
+                slow = slow.next
+            fast = fast.next
+        slow.next = None
+        return head
 # @lc code=end
 
