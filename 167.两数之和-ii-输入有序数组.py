900721d64f4472b58aa140ad460844a853d54c5a
Kun XIE <kunixe@tencent.com>
2022-11-14 11:21:26 +0800

    Add 344 左右指针 反转字符串

---
+++ 167.两数之和-ii-输入有序数组.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=167 lang=python3
+#
+# [167] 两数之和 II - 输入有序数组
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            curr_sum = numbers[left] + numbers[right]
+            if curr_sum == target:
+                return [left + 1, right + 1]
+            elif curr_sum < target:
+                left += 1
+            else:
+                right -= 1
+        return [-1, -1]
+# @lc code=end
+
