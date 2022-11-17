08c2a4ba31b7acec3114ac9d0a8ec3e1eb815af3
Kun XIE <kunixe@tencent.com>
2022-11-17 10:31:57 +0800

    Update ==> 46 更新回溯算法解法 全排列

--- 46.全排列.py
+++ 46.全排列.py
@@ -7,28 +7,40 @@
 # @lc code=start
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        """回溯算法: DFS Depth-First-Search"""
-        # 26/26 cases passed (32 ms)
-        # Your runtime beats 88.31 % of python3 submissions
-        # Your memory usage beats 45.62 % of python3 submissions (15.1 MB)
-        res = []
-        self.backtrack(res, [], nums)
-        return res
-
-    def backtrack(self, res, track, choice):
-        if len(track) == len(choice):
-            """必须用track[:]，否则track是空列表[]"""
-            res.append(track[:])
-        for c in choice:
-            # 排除不合理的选择
-            if c in track:
-                continue
+        """
+        回溯算法 DFS Depth-First-Search
+        for 选择 in 选择列表:
             # 做选择
-            track.append(c)
-            # 进入下一步决策树
-            self.backtrack(res, track, choice)
+            将该选择从选择列表移除
+            路径.add(选择)
+            backtrack(路径, 选择列表)
             # 撤销选择
-            track.pop()
+            路径.remove(选择)
+            将该选择再加入选择列表
+        """
+        def backtrack(res, track, choice):
+            """
+            res: 返回的结果
+            track: 当前路径，初始化是[]
+            choice: 路径可选集合
+            """
+            # 判断当前结果是否以满足条件
+            if len(track) == len(choice):
+                """必须用track[:]，否则track是空列表[]"""
+                # print(track)
+                res.append(track[:])
+            # 做选择
+            for c in choice:
+                # 判断当前选择是否已被使用：是否已在路径中(不合理的选择)
+                if c in track:
+                    continue
 
+                track.append(c)
+                backtrack(res, track, choice)
+                track.pop()
+        
+        res = []
+        backtrack(res, [], nums)
+        return res
 # @lc code=end
 
