4a197bba3e876d608d915355e550967ebedcd150
Kun XIE <kunixe@tencent.com>
2022-11-18 15:55:52 +0800

    Update ==> 76 滑动窗口 最小覆盖子串

--- 76.最小覆盖子串.py
+++ 76.最小覆盖子串.py
@@ -7,46 +7,41 @@
 # @lc code=start
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # 266/266 cases passed (104 ms)
-        # Your runtime beats 43.08 % of python3 submissions
-        # Your memory usage beats 92.85 % of python3 submissions (15 MB)
-        if not t or not s:
-            return ""
-        # 不可以用 window = need = {}
-        window, need = {}, {}
-        valid = 0
+        need, window = {}, {}
+        # 初始化need字段，每个字符分别需要出现几次
         for c in t:
             if c in need:
                 need[c] += 1
             else:
                 need[c] = 1
-        left = right = 0
+        valid = 0
+        left, right = 0, 0
         start, end = 0, len(s) + 1
-        # 滑动窗口：[start, right) 左闭右开
         while right < len(s):
+            # 增大滑动窗口：添加元素c
             c = s[right]
             right += 1
-            if c in window:
-                window[c] += 1
-            else:
-                window[c] = 1
-
-            if c in need and window[c] == need[c]:
-                valid += 1
+            # 更新窗口内数据
+            if c in need:
+                if c in window:
+                    window[c] += 1
+                else:
+                    window[c] = 1
+                if window[c] == need[c]:
+                    valid += 1
             
+            # 判断是否需要收缩窗口
             while valid == len(need):
+                # 更新最小覆盖子串
                 if right - left < end - start:
-                    start, end = left, right
-                
+                    start, end  = left, right
+                # 缩小滑动窗口：移除元素c
                 c = s[left]
                 left += 1
-                if c in need and window[c] == need[c]:
-                    valid -= 1
-                window[c] -= 1
-        
-        if end == len(s) + 1:
-            return ""
-        else:
-            return s[start:end]
+                if c in need:
+                    if window[c] == need[c]:
+                        valid -= 1
+                    window[c] -= 1
+        return s[start:end] if end != len(s) + 1 else ""
 # @lc code=end
 
