af2bedd1e07fa6e9857daa89cd7a93b022b09fa6
Kun XIE <kunixe@tencent.com>
2022-11-18 12:42:39 +0800

    Update ==> 704 二分查找

--- 704.二分查找.py
+++ 704.二分查找.py
@@ -7,18 +7,15 @@
 # @lc code=start
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # 46/46 cases passed (32 ms)
-        # Your runtime beats 96.15 % of python3 submissions
-        # Your memory usage beats 18.32 % of python3 submissions (15.9 MB)
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            med = (l + r) // 2
-            if nums[med] == target:
-                return med
-            if nums[med] > target:
-                r = med - 1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                right = mid - 1
             else:
-                l = med + 1
+                left = mid + 1
         return -1
 # @lc code=end
 
