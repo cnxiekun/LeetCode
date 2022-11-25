6020e873421899faaea242441b0ccf3c5267433c
Kun XIE <kunixe@tencent.com>
2022-11-25 11:52:27 +0800

    Update ==> 3 滑动窗口 无重复字符的最长子串

--- 3.无重复字符的最长子串.py
+++ 3.无重复字符的最长子串.py
@@ -7,20 +7,43 @@
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # 987/987 cases passed (48 ms)
-        # Your runtime beats 97.26 % of python3 submissions
-        # Your memory usage beats 98.88 % of python3 submissions (14.7 MB)
+        # if len(s) <= 1:
+        #     return len(s)
+        # res = 1
+        # curr = prev = s[0]
+        # for char in s[1:]:
+        #     if char in prev:
+        #         curr = prev[prev.index(char) + 1:] + char
+        #     else:
+        #         curr = prev + char
+        #     res = max(res, len(curr))
+        #     prev = curr
+        # return res
+        """
+        滑动窗口解法
+        """
         if len(s) <= 1:
             return len(s)
+        
         res = 1
-        curr = prev = s[0]
-        for char in s[1:]:
-            if char in prev:
-                curr = prev[prev.index(char) + 1:] + char
+        left, right = 0, 0
+        window = {}
+        # 增大窗口
+        while right < len(s):
+            c = s[right]
+            right += 1
+            if c in window:
+                window[c] += 1
             else:
-                curr = prev + char
-            res = max(res, len(curr))
-            prev = curr
+                window[c] = 1
+        
+            # 判断是否需要收缩窗口
+            while window[c] > 1:
+                d = s[left]
+                left += 1
+                window[d] -= 1
+            res = max(res, right - left)
+        
         return res
 # @lc code=end
 
