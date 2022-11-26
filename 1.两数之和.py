6bea6a7556322f9b2cbee5ebfb93b36fd63265e0
Kun XIE <kunixe@tencent.com>
2022-11-26 16:15:07 +0800

    Update ==> Hello World! 1 hash 表 两数之和

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -7,24 +7,15 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # 54/54 cases passed (28 ms)
-        # Your runtime beats 98.45 % of python3 submissions
-        # Your memory usage beats 10.12 % of python3 submissions (15.9 MB)
+        """
+        Hash表记录已经看到过的数值
+        """
         seen = {}
         for i, v in enumerate(nums):
             remaining = target - v
             if remaining in seen:
                 return [seen[remaining], i]
-            else:
-                seen[v] = i
-        return "No solution found"
-        # # 54/54 cases passed (3072 ms)
-        # # Your runtime beats 23.69 % of python3 submissions
-        # # Your memory usage beats 70.87 % of python3 submissions (15 MB)
-        # for i in range(len(nums) - 1):
-        #     for j in range(i+1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return [i, j]
-        # return "No solution found"
+            seen[v] = i
+        return None
 # @lc code=end
 
