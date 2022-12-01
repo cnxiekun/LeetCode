48b546c7cd4c5cc0ff2c1caad6b25a8809a1a603
Kun XIE <kunixe@tencent.com>
2022-12-01 12:48:47 +0800

    Update ==> 92 递归 反转链表-ii

--- 92.反转链表-ii.py
+++ 92.反转链表-ii.py
@@ -6,29 +6,31 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
-        # 44/44 cases passed (32 ms)
-        # Your runtime beats 77.66 % of python3 submissions
-        # Your memory usage beats 5.1 % of python3 submissions (15.5 MB)
         if left == 1:
             return self.reverseN(head, right)
         
-        self.last = None
+        # 跳过链表前left个节点：因为不需要做任何操作
         head.next = self.reverseBetween(head.next, left-1, right-1)
         return head
 
     def reverseN(self, head, n):
+        """
+        反转链表前N个节点
+        """
         if n == 1:
-            self.last = head.next
+            self.remianing = head.next
             return head
-        res = self.reverseN(head.next, n-1)
+        
+        last = self.reverseN(head.next, n-1)
         head.next.next = head
-        head.next = self.last
-        return res
+        head.next = self.remianing
+        return last
 # @lc code=end
 
