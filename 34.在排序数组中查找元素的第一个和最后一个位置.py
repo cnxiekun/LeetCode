bbb8a485076c7c6d5916e6a95a47f1f777b80678
Kun XIE <kunixe@tencent.com>
2022-12-02 11:38:55 +0800

    Update ==> 34 闭区间二分法 查找第一个和最后一个位置

--- 34.在排序数组中查找元素的第一个和最后一个位置.py
+++ 34.在排序数组中查找元素的第一个和最后一个位置.py
@@ -7,48 +7,29 @@
 # @lc code=start
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # # 88/88 cases passed (28 ms)
-        # # Your runtime beats 95.24 % of python3 submissions
-        # # Your memory usage beats 6.06 % of python3 submissions (15.8 MB)
-        # l, r = 0, len(nums) - 1
-        # while l <= r:
-        #     med = (l + r) // 2
-        #     if nums[med] < target:
-        #         l = med + 1
-        #     elif nums[med] > target:
-        #         r = med - 1
-        #     else:
-        #         while nums[l] != target:
-        #             l += 1
-        #         while nums[r] != target:
-        #             r -= 1
-        #         return [l, r]
-        # return [-1, -1]
-        """左右边界分别用二分法查找"""
-        # 88/88 cases passed (32 ms)
-        # Your runtime beats 85.57 % of python3 submissions
-        # Your memory usage beats 35.7 % of python3 submissions (15.7 MB)
-        def binarySearchLeft(nums, target):
-            l, r = 0, len(nums)-1
-            while l <= r:
-                med = (l+r) // 2
-                if nums[med] >= target:
-                    r = med - 1
-                else:
-                    l = med + 1
-            return l
+        left, right = self.binarySearchLeft(nums, target), self.binarySearchRight(nums, target)
+        return [left, right] if left <= right else [-1, -1]
+    
+    
+    def binarySearchLeft(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            med = (left + right) // 2
+            if nums[med] >= target:
+                right -= 1
+            else:
+                left += 1
+        return left
 
-        def binarySearchRight(nums, target):
-            l, r = 0, len(nums)-1
-            while l <= r:
-                med = (l+r) // 2
-                if nums[med] <= target:
-                    l = med + 1
-                else:
-                    r = med - 1
-            return r
-        
-        l, r = binarySearchLeft(nums, target), binarySearchRight(nums, target)
-        return [l, r] if l <= r else [-1, -1]
+    
+    def binarySearchRight(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            med = (left + right) // 2
+            if nums[med] <= target:
+                left += 1
+            else:
+                right -= 1
+        return right
 # @lc code=end
 
