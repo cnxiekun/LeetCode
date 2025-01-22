0f63da1a9ebfc65afeca8293dc47c01010f7a501
cnxiekun <cnxiekun@163.com>
2025-01-22 11:28:21 +0800

    Update ==> 删除重复&重命名文件

--- 88.合并两个有序数组.py
+++ 88.合并两个有序数组.py
@@ -1,33 +1,34 @@
-#
-# @lc app=leetcode.cn id=88 lang=python3
-#
-# [88] 合并两个有序数组
-#
+# 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。 
+# 
+#  说明: 
+# 
+#  
+#  初始化 nums1 和 nums2 的元素数量分别为 m 和 n。 
+#  你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。 
+#  
+# 
+#  示例: 
+# 
+#  输入:
+# nums1 = [1,2,3,0,0,0], m = 3
+# nums2 = [2,5,6],       n = 3
+# 
+# 输出: [1,2,2,3,5,6] 
+#  Related Topics 数组 双指针
 
-# @lc code=start
+
+# leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-        双指针从后往前，最大值放“当前”最后一位
         """
-        # 59/59 cases passed (28 ms)
-        # Your runtime beats 94.03 % of python3 submissions
-        # Your memory usage beats 23 % of python3 submissions (15 MB)
-        p1, p2, p3 = m-1, n-1, m+n-1
-        while p1 >=0 or p2 >= 0:
-            if p1 == -1:
-                nums1[p2] = nums2[p2]
-                p2 -= 1
-            elif p2 == -1:
-                p1 -= 1
-            elif nums2[p2] >= nums1[p1]:
-                nums1[p3] = nums2[p2]
-                p2 -= 1
-                p3 -=1
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
             else:
-                nums1[p3] = nums1[p1]
-                p1 -= 1
-                p3 -= 1
-# @lc code=end
-
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+        nums1[:n] = nums2[:n]
+# leetcode submit region end(Prohibit modification and deletion)
